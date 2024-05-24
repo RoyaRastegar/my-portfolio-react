ecedced4df1fa89bbc851f206e84206e4d3bbfcf
Roya Rastegar <rOYA.RASTEGAR54196@GMAIL.COM>
2024-05-24 14:04:30 +0800

    change hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,15 +4,13 @@ function Hero() {
   return (
     <div className="hero">
       <img src={PorofileImg} alt="porofile" />
-      <div className="hero-right">
-        <h1>
-          <span>I am Roya</span>,frontend developer
-        </h1>
-        <p>I am Iranian but I live in Malaysia.</p>
-        <div className="hero-action">
-          <div className="hero-connect">Connect with me</div>
-          <div className="hero-resume">My resume</div>
-        </div>
+      <h1>
+        <span>I am Roya</span>,frontend developer
+      </h1>
+      <p>I am Iranian but I live in Malaysia.</p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My resume</div>
       </div>
     </div>
   );
