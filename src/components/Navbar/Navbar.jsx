a056283049cdf60a9819026c7d361ac2fb73ee38
Roya Rastegar <rOYA.RASTEGAR54196@GMAIL.COM>
2024-06-05 18:43:52 +0800

    open&close menu function

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,38 @@
+import AnchorLink from "react-anchor-link-smooth-scroll";
 import "./Navbar.css";
 import PropTypes from "prop-types";
+import { TfiMenuAlt } from "react-icons/tfi";
+import { RiCloseLargeFill } from "react-icons/ri";
+import { useRef } from "react";
 
 const Navbar = ({ navItem }) => {
+  const menuRef = useRef();
+
+  const openMenu = () => {
+    menuRef.current.style.right = "0";
+  };
+  const closeMenu = () => {
+    menuRef.current.style.right = "-350px";
+  };
   return (
     <div className="navbar">
-      <ul className="nav-menu">
+      <TfiMenuAlt className="nav-mob-open" onClick={openMenu} />
+      <ul ref={menuRef} className="nav-menu">
+        <RiCloseLargeFill className="nav-mob-close" onClick={closeMenu} />
         {navItem.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li key={index}>
+            <AnchorLink className="anchor-link" offset={50} href={`#${item}`}>
+              {item}
+            </AnchorLink>
+          </li>
         ))}
       </ul>
-      <div className="nav-connect">Connect With Me</div>
+      <div className="nav-connect">
+        {" "}
+        <AnchorLink className="anchor-link" offset={50} href="#Contact">
+          Connect With Me
+        </AnchorLink>
+      </div>
     </div>
   );
 };
