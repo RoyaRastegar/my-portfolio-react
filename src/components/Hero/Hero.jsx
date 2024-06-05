a7bb145d844b7e14b724b174f6fd57b40acfce98
Roya Rastegar <rOYA.RASTEGAR54196@GMAIL.COM>
2024-06-05 18:45:01 +0800

    add anchor-link to navbar

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,21 @@
 import "./Hero.css";
 import PorofileImg from "../../assets/my image.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
 function Hero() {
   return (
-    <div className="hero">
+    <div id="Home" className="hero">
       <img src={PorofileImg} alt="porofile" />
       <h1>
         <span>I&apos;m Roya</span>,frontend developer
       </h1>
       <p>I&apos;m Iranian but I live in Malaysia.</p>
       <div className="hero-action">
-        <div className="hero-connect">Connect with me</div>
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#Contact">
+            Connect with me
+          </AnchorLink>
+        </div>
         <div className="hero-resume">My resume</div>
       </div>
     </div>
