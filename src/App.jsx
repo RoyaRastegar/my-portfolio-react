0103440ffa99dce88b9714a53a93851768a6b2c5
Roya Rastegar <rOYA.RASTEGAR54196@GMAIL.COM>
2024-06-05 18:47:09 +0800

    add navbar item

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Services from "./components/Services/Services";
 import MyWork from "./components/Work/MyWork";
 import Contact from "./components/Contact/Contact";
 const App = () => {
-  const navbarItem = ["About", "About me", "Services", "Portfolio", "Contact"];
+  const navbarItem = ["Home", "About", "Services", "Portfolio", "Contact"];
   return (
     <div>
       <Navbar navItem={navbarItem} />
