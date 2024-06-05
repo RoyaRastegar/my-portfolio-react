83bd1d75f75d8ff4e298ad060f131830b0a44873
Roya Rastegar <rOYA.RASTEGAR54196@GMAIL.COM>
2024-06-05 18:46:04 +0800

    add submit form and send to email with web3form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,9 +2,36 @@ import "./Contact.css";
 import { GoMail } from "react-icons/go";
 import { SlCallIn } from "react-icons/sl";
 import { SlLocationPin } from "react-icons/sl";
+import { useState } from "react";
 function Contact() {
+  const [result, setResult] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "55543864-4603-4bb3-b269-aa5a8a80eb1a");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      alert(result);
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+      alert(result);
+    }
+  };
+
   return (
-    <div className="contact">
+    <div id="Contact" className="contact">
       <div className="contact-title">
         <h1>Get in Touch</h1>
         <hr />
@@ -28,7 +55,7 @@ function Contact() {
             </div>
           </div>
         </div>
-        <form className="contact-right">
+        <form className="contact-right" onSubmit={onSubmit}>
           <label>Your Name:</label>
           <input type="text" name="name" placeholder="Please,Enter your name" />
           <label>Email:</label>
